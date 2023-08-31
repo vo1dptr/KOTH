e677a87262b9594abc453b1c8c0e22a798e46d40
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-08-31 18:40:39 +0100

    Add files via upload

---
+++ Run-Me-1Time.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# Run me script
+chmod +x Requirements.sh
+chmod +x Food/Food.sh
+chmod +x Fortune/Fortune.sh
+chmod +x Hackers/Hackers.sh
+chmod +x Lion/Lion.sh && chmod 600 Lion/id_rsa
+chmod +x Panda/Panda.sh
+chmod +x Production/Production.sh && chmod 600 Production/id_rsa
+chmod +x Shrek/Shrek.sh
+chmod +x Tyler/Tyler.sh
