0df2ee714ba1e4468d0be9f9cc3a2c455a2b7475
0x734 <123769326+H3X-007@users.noreply.github.com>
2023-09-06 13:58:07 +0100

    Update Run-Me-Only1Time.sh

--- Run-Me-Only1Time.sh
+++ Run-Me-Only1Time.sh
@@ -3,8 +3,8 @@
 # Run me Script
 
 chmod +x Requirements.sh
-chmod +x Food/Food.sh && chmod +x Food/Cracker.sh
-chmod +x Fortune/Fortune.sh
+chmod +x Food/Food.sh 
+chmod +x Fortune/Fortune.sh && chmod +x Fortune/Cracker.sh
 chmod +x Hackers/Hackers.sh
 chmod +x Lion/Lion.sh && chmod 600 Lion/id_rsa
 chmod +x Panda/Panda.sh
