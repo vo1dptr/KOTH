1a92932117d6ff38bd927638f152a3c66a78edf3
0x734 <123769326+H3X-007@users.noreply.github.com>
2023-09-07 20:40:37 +0100

    Update Run-Me-Only1Time.sh

--- Run-Me-Only1Time.sh
+++ Run-Me-Only1Time.sh
@@ -4,11 +4,11 @@
 
 chmod +x Requirements.sh
 chmod +x Food/Food.sh 
-chmod +x Fortune/Fortune.sh && chmod +x Fortune/Cracker.sh
+chmod +x Fortune/Fortune.sh 
 chmod +x Hackers/Hackers.sh
 chmod +x Lion/Lion.sh && chmod 600 Lion/id_rsa
 chmod +x Panda/Panda.sh
 chmod +x Production/Production.sh && chmod 600 Production/id_rsa
-chmod +x Hogwarts/Hogwarts.sh && chmod +x Hogwarts/TimeSaver.sh 
+chmod +x Hogwarts/Hogwarts.sh
 chmod +x Shrek/Shrek.sh
 chmod +x Tyler/Tyler.sh
